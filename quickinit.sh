767c2d02b726a73b27032ceb5467ed654b1c09bc
DiscDuck <77843475+DiscDuck@users.noreply.github.com>
2022-01-05 14:07:28 +0100

    add menu and functions

--- quickinit.sh
+++ quickinit.sh
@@ -1,18 +1,61 @@
 #!/bin/bash
 #
 
-echo "This script will upgrade your system and install Docker."
+# functions
+
+ask_reboot () {
+   echo
+   echo "Would you like to reboot now (y/n) ?"
+   read rebootchoice
+   if [ $rebootchoice = y ] || [ $rebootchoice = Y ]
+   then
+      sudo reboot
+   fi
+}
+
+update_upgrade () {
+   sudo apt update && sudo apt upgrade -y
+}
+
+install_docker () {
+   sudo apt install docker.io -y
+}
+
+#menu
+
 echo
-echo "Would you like to reboot at the end (recommended) y/n ?"
+echo "Ubuntu Init Menu"
+echo
+echo "1 Update and Upgrade the system"
+echo "2 Update and Upgrade the system and install Docker"
+echo "3 Install Docker"
+echo "9 Exit"
 
-read rebootchoice
+read menuchoice
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install docker.io -y
-
-echo "Upgrade and Docker install finished."
-
-if [ $rebootchoice = y ] || [ $rebootchoice = Y ]
+if [ $menuchoice = 1 ]
 then
-   sudo reboot
+   update_upgrade
+   echo
+   echo "Upgrade finished."
+   ask_reboot
+elif [ $menuchoice = 2 ]
+then
+   update_upgrade
+   install_docker
+   echo
+   echo "Upgrade and Docker install finished."
+   ask_reboot
+elif [ $menuchoice = 3 ]
+then
+   install_docker
+   echo
+   echo "Docker install finished."
+elif [ $menuchoice = 9 ]
+then
+   echo
+   echo "Exiting..."
+else
+   echo
+   echo "No valid choice given."
 fi
